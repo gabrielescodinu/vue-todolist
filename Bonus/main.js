ef938ed02777af3185c8e4f33bad3ac56af69eaf
gabriele <gabrielescodinu96@gmail.com>
2020-12-17 10:24:17 +0100

    commit 10

--- Bonus/main.js
+++ Bonus/main.js
@@ -7,6 +7,13 @@
 // Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il tasto Enter della tastiera.
 // Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
 
+// Istruzioni Bonus (da mettere in una cartella bonus):
+// L'utente vuole poter modificare una task giá inserita
+// ma vuole anche poter indicare che la task é stata completata
+// inoltre se una task é stata completa allora vuole che venga inserita in un'altra colonna tipo "tasks completate"
+// ah non é finita, dice che quando cancella una task, non vuole che questa venga subito rimossa, ma vuole che resti visibile ma venga spostata in una colonna tipo "cestino"
+// si, l'utente é un ropi scatole, dice infine che vuole poter rimuovere tutte le tasks nel cestino cliccando su un pulsante tipo "svuota cestino"
+
 let root = new Vue ({
   el: "#root",
   data: {
@@ -22,7 +29,6 @@ let root = new Vue ({
     remove(index){
       this.completati.push(this.lista[index]);
       this.lista.splice(index, 1);
-      console.log(this.lista[index]);
     },
     add(){
       if (this.testo.length > 3) {
@@ -30,6 +36,10 @@ let root = new Vue ({
         this.lista.push(this.testo);
         this.testo = "";
       }
+    },
+    deleteAll(index){
+      this.completati.splice(index);
+      console.log(this.completati);
     },
   }
 });
